de51d45c1e60ecbd0299561f964c1b07d1d12fa1
mahdoosh1 <83725163+mahdoosh1@users.noreply.github.com>
2023-11-16 17:09:35 +0330

    update variable names

--- engine.py
+++ engine.py
@@ -1,4 +1,4 @@
-def ex2(arr):
+def extent2d(arr):
 	y = len(arr)
 	x = len(arr[0])
 	new = [[False for _ in range(x+2)] for _ in range(y+2)]
@@ -6,54 +6,52 @@ def ex2(arr):
 		for i in range(x+2):
 			new[j][i] = arr[(j-1)%y][(i-1)%x]
 	return new
-def neigh(arr,idx,idy,typ=8,ext=False):
+def neigh(arr,idx,idy,typ=8,extent=False):
 	y = len(arr)
 	x = len(arr[0])
-	f = int(ext)
+	f = int(extent)
 	if typ == 8 and (idx+2+f)%x > (idx-1+f)%x:
 		t = arr[(idy-1+f)%y][(idx-1+f)%x:(idx+2+f)%x]
 		m = arr[idy+f][(idx-1+f)%x:(idx+2+f)%x]
 		b = arr[(idy+1+f)%y][(idx-1+f)%x:(idx+2+f)%x]
 		out = sum(t+m+b)-arr[idy+f][idx+f]
 	else:
-		if not ext:
-			ext = []
-			for i in ex2(arr):
-				ext.append(arrint(i))
+		if not extent:
+			extent = []
+			for i in extent2d(arr):
+				extent.append(arrint(i))
 		else:
-			ext = arr
-		ne = ext[idy][idx] if (typ!=4) else 0
-		n = ext[idy][idx+1]
-		nw = ext[idy][idx+2] if (typ!=4) else 0
-		e = ext[idy+1][idx]
-		w = ext[idy+1][idx+2]
-		se = ext[idy+2][idx] if (typ!=4) else 0
-		s = ext[idy+2][idx+1]
-		sw = ext[idy+2][idx+2] if (typ!=4) else 0
+			extent = arr
+		ne = extent[idy][idx] if (typ!=4) else 0
+		n = extent[idy][idx+1]
+		nw = extent[idy][idx+2] if (typ!=4) else 0
+		e = extent[idy+1][idx]
+		w = extent[idy+1][idx+2]
+		se = extent[idy+2][idx] if (typ!=4) else 0
+		s = extent[idy+2][idx+1]
+		sw = extent[idy+2][idx+2] if (typ!=4) else 0
 		out = (ne+n+nw+e+w+se+s+sw)
 	return out
 def arrint(arr):
 	return [int(i) for i in list(arr)]
-def cnst(y,x):
+def construct(y,x):
 	return [[None for _ in range(x)] for _ in range(y)]
-def nxt(arr,rule):
-	ext = []
-	for i in ex2(arr):
-		ext.append(arrint(i))
+def nextframe(arr,rule):
+	extent = []
+	for i in extent2d(arr):
+		extent.append(arrint(i))
 	typ = rule["type"]
 	born = arrint(rule["born"])
 	surv = arrint(rule["survive"])
 	y = len(arr)
 	x = len(arr[0])
-	new = cnst(y,x)
-	ngh = cnst(y,x)
+	new = construct(y,x)
 	for j in range(y):
 		for i in range(x):
-			c = arr[j][i]
-			n = neigh(ext,i,j,typ,ext=True)
-			c = int(n in surv) if c else int(n in born)
-			ngh[j][i] = n
-			new[j][i] = c
+			cell = arr[j][i]
+			n = neigh(extent,i,j,typ,extent=True)
+			cell = int(n in surv) if cell else int(n in born)
+			new[j][i] = cell
 	return new
 CONWAY = {
 "type":8,
--- main.py
+++ main.py
@@ -4,80 +4,80 @@ import engine as gol
 import random as rnd
 from time import sleep
 pygame.init()
-arr = [[rnd.randint(0,1) for _ in range(50)] for _ in range(50)]
-y=len(arr)
-x=len(arr[0])
-sx, sy = 710, 710
-sf = sx / x
-rsf = 1 / sf
-f = 9 / 10
-spd = 1
-p = False
+field = [[rnd.randint(0,1) for _ in range(50)] for _ in range(50)]
+y=len(field)
+x=len(field[0])
+screen_width, screen_height = 710, 710
+scale_factor = screen_width / x
+reverse_scale_factor = 1 / scale_factor
+gridfilling = 9 / 10
+speed = 1
+unpaused = False
 rule = gol.DAYNIGHT
-surface = pygame.display.set_mode((sx,sy))
-surface.fill((30,30,30))
-def drw(scrn, arr, upd = True, rule = gol.CONWAY):
-	global sf
-	y=len(arr)
-	x=len(arr[0])
-	if upd:
-		arr = gol.nxt(arr,rule)
+screen = pygame.display.set_mode((screen_width,screen_height))
+screen.fill((30,30,30))
+def drawframe(screen, field, updated = True, rule = gol.CONWAY):
+	global scale_factor
+	y=len(field)
+	x=len(field[0])
+	if updated:
+		field = gol.nextframe(field,rule)
 	for i in range(y):
 		for j in range(x):
-			c = arr[i][j]
-			pygame.draw.rect(scrn, (255*c,255*c,255*c), pygame.Rect(j*sf, i*sf, f*sf, f*sf))
-	return arr
+			c = field[i][j]
+			pygame.draw.rect(screen, (255*c,255*c,255*c), pygame.Rect(j*scale_factor, i*scale_factor, gridfilling*scale_factor, gridfilling*scale_factor))
+	return field
 print("Ready.")
 print("Hello! this is a game of life simulator.")
 print("Keys:\n           w: speed up,\n           s: speed down")
 print("           a: start simulation,\n           d: step once")
 print("           c: clear,\n           r: fill with random")
 print("           e: toggle between Conway's GOL and day and night gol")
-drw(surface,arr,False)
+drawframe(screen,field,False)
 pygame.display.flip()
 while True:
-	for ev in pygame.event.get():
-		if ev.type==pygame.QUIT:
+	for event in pygame.event.get():
+		if event.type==pygame.QUIT:
 			pygame.quit()
-		if ev.type == pygame.MOUSEBUTTONDOWN:
-			(mx, my) = pygame.mouse.get_pos()
-			cx, cy = int(mx*rsf), int(my*rsf)
-			if cx < x and cy < y:
-				arr[cy][cx] = 1-arr[cy][cx]
-				drw(surface,arr,False)
+		if event.type == pygame.MOUSEBUTTONDOWN:
+			(mousex, mousey) = pygame.mouse.get_pos()
+			clickx, clicky = int(mx*reverse_scale_factor), int(my*reverse_scale_factor)
+			if clickx < x and clicky < y:
+				field[clicky][clickx] = 1-field[clicky][clickx]
+				drawframe(screen,field,False)
 				pygame.display.flip()
-		if ev.type == pygame.KEYDOWN:
-			if ev.key == pygame.K_d:
-				arr = drw(surface,arr,rule=rule)
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_d:
+				field = drawframe(screen,field,rule=rule)
 				pygame.display.flip()
-			if ev.key == pygame.K_a:
-				p = not p
-			if ev.key == pygame.K_w:
-				spd = spd * 2
-			if ev.key == pygame.K_s:
-				spd = spd / 2
-			if ev.key == pygame.K_e:
+			if event.key == pygame.K_a:
+				unpaused = not unpaused
+			if event.key == pygame.K_w:
+				speed = speed * 2
+			if event.key == pygame.K_s:
+				speed = speed / 2
+			if event.key == pygame.K_e:
 				if rule == gol.DAYNIGHT:
 					rule = gol.CONWAY
 				else:
 					rule = gol.DAYNIGHT
-			if ev.key == pygame.K_r:
-				arr = [[rnd.randint(0,1) for _ in range(50)] for _ in range(50)]
-				drw(surface,arr,False)
+			if event.key == pygame.K_r:
+				field = [[rnd.randint(0,1) for _ in range(50)] for _ in range(50)]
+				drawframe(screen,field,False)
 				pygame.display.flip()
-			if ev.key == pygame.K_c:
-				arr = [[0 for _ in range(50)] for _ in range(50)]
-				drw(surface,arr,False)
+			if event.key == pygame.K_c:
+				field = [[0 for _ in range(50)] for _ in range(50)]
+				drawframe(screen,field,False)
 				pygame.display.flip()
-			if ev.key == pygame.K_q:
+			if event.key == pygame.K_q:
 				pygame.quit()
 				quit()
-	if p:
-		if spd >=1:
-			for _ in range(int(spd)):
-				arr = drw(surface,arr,rule=rule)
+	if unpaused:
+		if speed >=1:
+			for _ in range(int(speed)):
+				field = drawframe(screen,field,rule=rule)
 				pygame.display.flip()
 		else:
-			sleep(1/128/spd)
-			arr = drw(surface,arr,rule=rule)
+			sleep(1/128/speed)
+			field = drawframe(screen,field,rule=rule)
 			pygame.display.flip()
